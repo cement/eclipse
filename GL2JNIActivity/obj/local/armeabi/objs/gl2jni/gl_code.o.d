750f65fa243d6d6f8ebcbd92f5e8bea533ed90eb
cement <32463636@qq.com>
2016-10-04 04:46:13 +0800

    add

---
+++ GL2JNIActivity/obj/local/armeabi/objs/gl2jni/gl_code.o.d
@@ -0,0 +1,109 @@
+obj/local/armeabi/objs/gl2jni/gl_code.o: jni/gl_code.cpp \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/jni.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/android/api-level.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/android/log.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/GLES2/gl2.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/GLES2/gl2platform.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/stdint.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/GLES2/gl2ext.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/stdio.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/string.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/malloc.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/alloca.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/strings.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/memory.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/math.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/limits.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/syslimits.h \
+ E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/asm/page.h
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/jni.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/android/api-level.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/android/log.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/GLES2/gl2.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/GLES2/gl2platform.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/GLES2/gl2ext.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/stdio.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/string.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/strings.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/memory.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/math.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/limits.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
+
+E:/eclipse/android-ndk-r9d-windows-x86/android-ndk-r9d/platforms/android-5/arch-arm/usr/include/asm/page.h:
