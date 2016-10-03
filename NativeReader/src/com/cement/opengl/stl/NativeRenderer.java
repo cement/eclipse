d9574d83f9b135f0d3527e77022afb8c1d4ffd1c
cement <32463636@qq.com>
2016-10-04 03:52:48 +0800

    aaa

--- NativeReader/src/com/cement/opengl/stl/NativeRenderer.java
+++ NativeReader/src/com/cement/opengl/stl/NativeRenderer.java
@@ -4,6 +4,4 @@ public class NativeRenderer {
 	public native void nativeSurfaceDrawing();
 	public native void nativeSurfaceCreated();
 	public native void nativeSurfacechanged();
-	public native void nativeSurfacechanged2();
-	public native void nativeSurfacechanged3();
 }
